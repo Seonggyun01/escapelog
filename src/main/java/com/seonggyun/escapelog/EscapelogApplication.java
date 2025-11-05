9fbc3ae0a9898550dbe8d257315ddea45902cf42
Seonggyun01 <thdrbs010314@naver.com>
2025-11-06 00:57:41 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/seonggyun/escapelog/EscapelogApplication.java
@@ -0,0 +1,13 @@
+package com.seonggyun.escapelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapelogApplication.class, args);
+	}
+
+}
