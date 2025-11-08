667cabf8c427eeab69073056f47633d4c0f52d0d
Seonggyun01 <thdrbs010314@naver.com>
2025-11-09 00:13:08 +0900

    feat(service): MemberService 클래스 추가
    - 회원 가입 기능
    - 전체 조회 기능
    - 단건 조회
    - 이름으로 회원 조회

--- src/main/java/com/seonggyun/escapelog/service/MemberService.java
+++ src/main/java/com/seonggyun/escapelog/service/MemberService.java
@@ -1,4 +1,46 @@
 package com.seonggyun.escapelog.service;
 
+import com.seonggyun.escapelog.domain.Member;
+import com.seonggyun.escapelog.repository.MemberRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입(회원 추가)
+     */
+    @Transactional
+    public Long join(String name){
+        Member member = new Member(name);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 전체 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 단건 조회
+     */
+    public Member findOne(Long memberId){
+        return memberRepository.findById(memberId).get();
+    }
+
+    /**
+     * 이름으로 회원 조회
+     */
+    public List<Member> findByName(String name) {
+        return memberRepository.findByName(name);
+    }
 }
