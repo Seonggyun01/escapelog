28a58c3b742b0b6c7940be2ea255c3d0b5a71833
Seonggyun01 <thdrbs010314@naver.com>
2025-11-09 00:10:53 +0900

    feat(repository): findByMemberId(), findByThemeId() 메서드 추가
    - 맴버 아이디로 조회 기능
    - 테마 아이도로 조회 기능

--- src/main/java/com/seonggyun/escapelog/repository/PlayRecordRepository.java
+++ src/main/java/com/seonggyun/escapelog/repository/PlayRecordRepository.java
@@ -1,7 +1,10 @@
 package com.seonggyun.escapelog.repository;
 
 import com.seonggyun.escapelog.domain.PlayRecord;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayRecordRepository extends JpaRepository<PlayRecord, Long> {
+    List<PlayRecord> findByMemberId(Long memberId);
+    List<PlayRecord> findByThemeId(Long themeId);
 }
