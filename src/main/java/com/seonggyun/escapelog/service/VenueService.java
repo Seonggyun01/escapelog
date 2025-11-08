3db690065558bf84454ddff521d5883cfcb82f8c
Seonggyun01 <thdrbs010314@naver.com>
2025-11-09 00:14:47 +0900

    feat(Service): VenueService 클래스 추가
    - 매장 등록 기능
    - 전체 조회 기능
    - 단건 조회 기능
    - 매장명으로 조회 기능

--- src/main/java/com/seonggyun/escapelog/service/VenueService.java
+++ src/main/java/com/seonggyun/escapelog/service/VenueService.java
@@ -1,4 +1,46 @@
 package com.seonggyun.escapelog.service;
 
+import com.seonggyun.escapelog.domain.Venue;
+import com.seonggyun.escapelog.repository.VenueRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class VenueService {
+    private final VenueRepository venueRepository;
+
+    /**
+     * 매장 등록
+     */
+    @Transactional
+    public Long saveVenue(String name, String region){
+        Venue venue = new Venue(name, region);
+        venueRepository.save(venue);
+        return venue.getId();
+    }
+
+    /**
+     * 전체 조회
+     */
+    public List<Venue> findAll(){
+        return venueRepository.findAll();
+    }
+
+    /**
+     * 단건 조회
+     */
+    public Venue findOne(Long id){
+        return venueRepository.findById(id).get();
+    }
+
+    /**
+     * 매장명 조회
+     */
+    public List<Venue> findByName(String name) {
+        return venueRepository.findByNameContainingIgnoreCase(name);
+    }
 }
