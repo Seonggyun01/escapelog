ada7d074dfaceb4fe5562407217bcb673db710fc
Seonggyun01 <thdrbs010314@naver.com>
2025-11-15 16:07:39 +0900

    feat(service): MemberService 회원 가입, 로그인 기능 추가
    - join(): 아이디, 비밀번호, 이름(닉네임)으로 회원 가입
              아이디, 이름 중복 검사
    - findByName(): 이름으로 회원 찾기
    - findByLoginId(): 아이디로 회원 찾기
    - login(): 아이디로 회원 조회, 회원이 없으면 예외 발생
               비밀번호 체크, 맞으면 회원 반환, 틀리면 null 반환

--- src/main/java/com/seonggyun/escapelog/service/MemberService.java
+++ src/main/java/com/seonggyun/escapelog/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.seonggyun.escapelog.service;
 
+import com.seonggyun.escapelog.AuthException;
 import com.seonggyun.escapelog.domain.Member;
 import com.seonggyun.escapelog.repository.MemberRepository;
 import java.util.List;
@@ -17,10 +18,26 @@ public class MemberService {
      * 회원 가입(회원 추가)
      */
     @Transactional
-    public Long join(String name){
-        Member member = new Member(name);
+    public Long join(String loginId, String password, String name){
+        validateDuplicateLoginId(loginId);
+        validateDuplicateName(name);
+        Member member = new Member(loginId, password, name);
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateLoginId(String loginId) {
+        memberRepository.findByLoginId(loginId)
+                .ifPresent(m -> {
+                    throw new IllegalArgumentException("이미 사용 중인 로그인 ID입니다.");
+                });
+    }
+
+    private void validateDuplicateName(String name) {
+        memberRepository.findByName(name)
+                .ifPresent(m -> {
+                    throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+                });
     }
 
     /**
@@ -33,14 +50,38 @@ public class MemberService {
     /**
      * 단건 조회
      */
-    public Member findOne(Long memberId){
-        return memberRepository.findById(memberId).get();
+    public Member findOne(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
     }
 
     /**
-     * 이름으로 회원 조회
+     * 닉네임으로 단일 회원 조회 (name이 unique라는 전제)
      */
-    public List<Member> findByName(String name) {
-        return memberRepository.findByName(name);
+    public Member findByName(String name) {
+        return memberRepository.findByName(name)
+                .orElseThrow(() -> new IllegalArgumentException("해당 닉네임의 회원이 없습니다."));
+    }
+
+    /**
+     * loginId로 단일 회원 조회
+     */
+    public Member findByLoginId(String loginId) {
+        return memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 회원이 없습니다."));
+    }
+
+    /**
+     * login 검증
+     */
+    public Member login(String loginId, String password) {
+
+        Member loginMember = memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new AuthException("이미 가입안된 사람입니다."));
+
+        if(loginMember.getPassword().equals(password)){
+            return loginMember;
+        }
+        return null;
     }
 }
