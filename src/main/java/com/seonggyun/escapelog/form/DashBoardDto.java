c6431450395a154947f0ab54db167dd6cde4ab38
Seonggyun01 <thdrbs010314@naver.com>
2025-11-16 19:07:16 +0900

    feat(controller): 대시보드 화면 컨트롤러 및 DTO 추가
    - /dashboard 요청을 처리하는 DashboardController 구현
    - 세션의 로그인 회원 정보로 통계 데이터 조회 후 모델에 담아 전달
    - 총 플레이 수, 성공률, 방문 매장 수, 평균 클리어 시간을 담는 DashBoardDto 추가
    - 최근 플레이 기록 리스트를 모델에 추가하여 화면에서 사용 가능하도록 구성

--- src/main/java/com/seonggyun/escapelog/form/DashBoardDto.java
+++ src/main/java/com/seonggyun/escapelog/form/DashBoardDto.java
@@ -1,4 +1,13 @@
 package com.seonggyun.escapelog.form;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class DashBoardDto {
+    private long totalPlays;
+    private Integer successRate;
+    private long storeCount;
+    private String avgTime;
 }
